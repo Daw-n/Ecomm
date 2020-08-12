805f29253648123d55dd03f474841be36bfa8ffd
Dawn <56597380+Daw-n@users.noreply.github.com>
2020-08-12 14:18:24 +0500

    Update body.php
    changed carousel pics

--- body.php
+++ body.php
@@ -10,25 +10,25 @@
     <div class="carousel-inner">
 
       <div class="item active">
-        <img src="img/banner3.jpg" alt="Los Angeles" style="width:100%;">
+        <img src="img/pic5.jpg" style="width:100%;">
         
       </div>
 
       <div class="item">
-        <img src="img/banner2.jpg" style="width:100%;">
+        <img src="img/pic2.jpg" style="width:100%;">
         
       </div>
     
       <div class="item">
-        <img src="img/banner4.jpg" alt="New York" style="width:100%;">
+        <img src="img/pic1.jpg" alt="New York" style="width:100%;">
         
       </div>
       <div class="item">
-        <img src="img/banner1.jpg" alt="New York" style="width:100%;">
+        <img src="img/pic6.jpg" alt="New York" style="width:100%;">
         
       </div>
       <div class="item">
-        <img src="img/banner3.jpg" alt="New York" style="width:100%;">
+        <img src="img/pic5.jpg" alt="New York" style="width:100%;">
         
       </div>
   
